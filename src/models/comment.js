21dc183b35d9c90c76a803152a055520432f25fe
HUN1i <nicebrian0328@gmail.com>
2023-09-18 11:28:46 +0900

    FIX :: comment 테이블의 고유 ID를 1로 설정

--- src/models/comment.js
+++ src/models/comment.js
@@ -6,6 +6,7 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true,
+        default: 1,
       },
       tableId: {
         type: DataTypes.INTEGER,
